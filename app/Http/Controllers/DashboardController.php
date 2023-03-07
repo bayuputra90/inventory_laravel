508adf6862d95c17ecf4bf9562bdbe15eaae3321
bayuputra90 <bputra2212@gmail.com>
2023-03-07 08:59:38 +0700

    Table permohonan pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permohonan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.home');
+        $permohonan = Permohonan::orderBy('created_at', 'desc')->limit(4)->get();
+
+        foreach ($permohonan as $data) {
+            $data['color_status'] = $this->color_status($data->status);
+        }
+
+        return view('dashboard.home', ['permohonan' => $permohonan]);
     }
 }
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,8 +4,8 @@
 <div class="row">
     <div class="col-md-6">
         <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Chart Barang</h3>
+            <div class="card-header bg-primary">
+                <h3 class="card-title text-light">Chart Barang</h3>
             </div>
             <div class="card-body">
                 chart here <br><br><br>
@@ -14,11 +14,28 @@
     </div>
     <div class="col-md-6">
         <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Permohonan Barang</h3>
+            <div class="card-header bg-primary">
+                <h3 class="card-title text-light">Permohonan Barang</h3>
             </div>
             <div class="card-body">
-                table here <br><br><br>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr class="table-success">
+                            <th>Permohonan</th>
+                            <th>Status</th>
+                            <th>Tanggal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($permohonan as $data)
+                        <tr>
+                            <td>{{ $data->judul }}</td>
+                            <td><span class="badge text-bg-{{ $data->color_status }}">{{ $data->status }}</span></td>
+                            <td>{{ date('d M Y', strtotime($data->created_at)) }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
